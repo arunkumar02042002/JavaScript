dabf9e83c1d0e3d573bb795fc79d5a7dae706ed2
= <arun.kumar.2403gg@gmail.com>
2023-09-04 10:14:11 +0530

    JS Variables

---
+++ 1. Basics of JS/helloWorld.js
@@ -0,0 +1,16 @@
+// Console is an API provided by the browsers
+console.log("Hello World!");
+
+var message = "Hello World!";
+console.log(message);
+
+var message = "Hello Arun!"; // Can create same variable usinh var
+console.log(message);
+
+let message2 = "Learning Js";
+console.log(message2);
+
+// let message2 ="JS is Best"; // Gives error
+
+const discount = 0.3; // Cannot be assigned twice
+console.log(discount)
